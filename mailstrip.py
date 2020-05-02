e56caa5a0dedd41d76bc7bf0a8f9df792c31eefc
Pontus Lurcock <pont@talvi.net>
2020-05-02 15:55:19 +0200

    Improve code style

--- mailstrip.py
+++ mailstrip.py
@@ -1,26 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-'Print the important bits of an email message'
+"""Print the important bits of an email message"""
 
-import email, sys
+import email
+import sys
 
-message = email.message_from_file(sys.stdin)
-for header in 'from to subject date'.split(' '):
-    if header in message:
-        print header.capitalize()+': '+message[header]
 
-payload = message
-while (type(payload) != str):
-    payload = payload.get_payload(decode = not payload.is_multipart())
-    if (type(payload) == list): payload = payload[0]
+def main():
+    message = email.message_from_file(sys.stdin)
+    for header in "from", "to", "subject", "date":
+        if header in message:
+            print header.capitalize() + ": " + message[header]
 
-print
-css = []
-cs = message.get_content_charset()
-if (cs==None):
-    # the filter removes the 'None' elements (since None is false)
-    css = filter(None, message.get_charsets())
-    if len(css)>0: cs = css[0]
-    else: cs = 'ascii' # fallback
-payload_u = unicode(payload, cs)
-print payload_u.encode('utf-8')
+    payload = message
+    while type(payload) != str:
+        payload = payload.get_payload(decode=not payload.is_multipart())
+        if type(payload) == list:
+            payload = payload[0]
+
+    print
+
+    charset = message.get_content_charset()
+    if charset is None:
+        # the filter removes the "None" elements (since None is false)
+        charsets = filter(None, message.get_charsets())
+        if len(charsets) > 0:
+            charset = charsets[0]
+        else:
+            charset = "ascii"  # fallback
+    payload_u = unicode(payload, charset)
+    print payload_u.encode("utf-8")
+
+
+if __name__ == "__main__":
+    main()
