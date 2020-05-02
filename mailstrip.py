a9b705d29faa7cb4e1c6655b4ce4446970d7a6e1
Pontus Lurcock <pont@talvi.net>
2020-05-02 16:44:56 +0200

    Convert to Python 3 and refactor

--- mailstrip.py
+++ mailstrip.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-"""Print the important bits of an email message"""
+"""Print the important bits of an email message.
+
+Raw email message is read from standard input.
+A plain text representation of selected message headers and content
+is written to standard output.
+
+By Pontus Lurcock, 2020. Released into the public domain.
+"""
 
 import email
 import sys
@@ -8,28 +15,38 @@ import sys
 
 def main():
     message = email.message_from_file(sys.stdin)
+    print_headers(message)
+    print()
+    payload = extract_payload(message)
+    charset = get_charset(message)
+    print(str(payload, encoding=charset))
+
+
+def print_headers(message):
     for header in "from", "to", "subject", "date":
         if header in message:
-            print header.capitalize() + ": " + message[header]
+            print(header.capitalize() + ": " + message[header])
 
+
+def extract_payload(message):
     payload = message
-    while type(payload) != str:
+    while type(payload) == email.message.Message:
         payload = payload.get_payload(decode=not payload.is_multipart())
         if type(payload) == list:
             payload = payload[0]
+    return payload
 
-    print
 
+def get_charset(message):
     charset = message.get_content_charset()
     if charset is None:
         # the filter removes the "None" elements (since None is false)
-        charsets = filter(None, message.get_charsets())
+        charsets = list(filter(None, message.get_charsets()))
         if len(charsets) > 0:
             charset = charsets[0]
         else:
             charset = "ascii"  # fallback
-    payload_u = unicode(payload, charset)
-    print payload_u.encode("utf-8")
+    return charset
 
 
 if __name__ == "__main__":
