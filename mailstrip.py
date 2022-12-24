e54cfed36a3111970ffec116d8bd237307f74103
Pontus Lurcock <pont@talvi.net>
2022-12-24 10:41:46 +0000

    Remove extract_payload and get_charset functions
    extract_payload replaced with the Python email library's get_body
    method. get_charset was no longer being used (again, replaced by
    functionality which the email library has acquired since the initial
    version of this script).
    
    This script is now a very thin wrapper around the email library.

--- mailstrip.py
+++ mailstrip.py
@@ -18,35 +18,14 @@ def main():
     message = email.message_from_file(sys.stdin, policy=email.policy.SMTP)
     print_headers(message)
     print()
-    payload = extract_payload(message)
-    print(payload)
+    body = message.get_body(preferencelist=("plain", "html", "related"))
+    print(body.get_content())
 
 
 def print_headers(message):
     for header in "from", "to", "subject", "date":
         if header in message:
             print(header.capitalize() + ": " + message[header])
-
-
-def extract_payload(message):
-    payload = message
-    while type(payload) in (email.message.Message, email.message.EmailMessage):
-        payload = payload.get_payload()
-        if type(payload) == list:
-            payload = payload[0]
-    return payload
-
-
-def get_charset(message):
-    charset = message.get_content_charset()
-    if charset is None:
-        # the filter removes the "None" elements (since None is false)
-        charsets = list(filter(None, message.get_charsets()))
-        if len(charsets) > 0:
-            charset = charsets[0]
-        else:
-            charset = "ascii"  # fallback
-    return charset
 
 
 if __name__ == "__main__":
